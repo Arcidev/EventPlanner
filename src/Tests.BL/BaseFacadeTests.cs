4542562ef917f52e126ab95689f3f7fee0d9a8a8
Arci <Arcidev@users.noreply.github.com>
2016-11-11 18:42:31 +0100

    Tests: added test project for BL with DI configured

---
+++ src/Tests.BL/BaseFacadeTests.cs
@@ -0,0 +1,23 @@
+﻿using EventPlanner.BL.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Tests.BL
+{
+    public abstract class BaseFacadeTests
+    {
+        private IServiceCollection services;
+        protected IServiceProvider serviceProvider;
+
+        public BaseFacadeTests()
+        {
+            EventPlanner.BL.Configuration.AutoMapper.Init();
+            services = new ServiceCollection();
+            services.ConfigureBLServices();
+            ConfigureFacade(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected abstract void ConfigureFacade(IServiceCollection services);
+    }
+}
---
+++ src/Tests.BL/UserFacadeTests.cs
@@ -0,0 +1,25 @@
+﻿using EventPlanner.BL.Facades.Interfaces;
+using EventPlanner.BL.Facades;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.BL
+{
+    public class UserFacadeTests : BaseFacadeTests
+    {
+        protected override void ConfigureFacade(IServiceCollection services)
+        {
+            services.AddTransient<IUserFacade, UserFacade>();
+        }
+
+        [Fact]
+        public async Task TestUserFacade()
+        {
+            var userFacade = serviceProvider.GetRequiredService<IUserFacade>();
+            var user = await userFacade.CreateOrGetUser("test@mail.sk");
+            Assert.NotNull(user);
+            Assert.Equal("test@mail.sk", user.Email);
+        }
+    }
+}
