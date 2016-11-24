9bc88626eb43a14477af1af1766c7e899f0b981b
Roman Luks <rluks@seznam.cz>
2016-11-24 16:39:54 +0100

    event edit - added people,datetime

--- src/EventPlanner.UI/Views/Home/eventEdit.jsx
+++ src/EventPlanner.UI/Views/Home/eventEdit.jsx
@@ -10,25 +10,54 @@ import '../../Styles/site.css';
 class EventEditLayout extends React.Component {
     render() {
         return (
-            <form class="form-horizontal">
-            <div class="form-group">
-                <label for="eventName" class="col-sm-2 control-label">Name</label>
-                <div class="col-sm-10">
-                <input type="text" id="eventName" class="form-control" placeholder="Event name" />
+            <div>
+            <h2>Basic information</h2>
+                <div>
+                <form class="form-horizontal">
+                <div class="form-group">
+                    <label for="eventName" class="col-sm-2 control-label">Name</label>
+                    <div class="col-sm-10">
+                    <input type="text" id="eventName" class="form-control" placeholder="Event name" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="eventDesc" class="col-sm-2 control-label">Description</label>
+                    <div class="col-sm-10">
+                    <textarea id="eventDesc" class="form-control" rows="3"></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-default">Save event</button>
+                    </div>
+                </div>
+                </form>
+                </div>
+            <h2>People</h2>
+                <div>
+                <form class="form-horizontal">
+                    <button type="button" class="btn btn-default">Add people</button>
+                    <div class="form-group">
+                        <label for="personEmail0" class="col-sm-2 control-label">Person's email</label>
+                        <div class="col-sm-10">
+                        <input type="text" id="personEmail0" class="form-control" placeholder="john.smith@example.com" />
+                        </div>
+                    </div>
+                </form>
+                </div>
+            <h2>Date and Time</h2>
+                <div>
+                <form class="form-horizontal">
+                    <button type="button" class="btn btn-default">Add date</button>
+                    <div class="form-group">
+                        <label for="eventDate0" class="col-sm-2 control-label">Datetime</label>
+                        <div class="col-sm-10">
+                        <input type="datetime-local" id="eventDate0" class="form-control" />
+                        </div>
+                    </div>
+                </form>
                 </div>
             </div>
-            <div class="form-group">
-                <label for="eventDesc" class="col-sm-2 control-label">Description</label>
-                <div class="col-sm-10">
-                <textarea id="eventDesc" class="form-control" rows="3"></textarea>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Save event</button>
-                </div>
-            </div>
-            </form>
         );
     }
 }
