145dfbff4db3f7077628d87623bd18c761198284
Arci <Arcidev@users.noreply.github.com>
2016-12-09 01:29:20 +0100

    Api/EventData: Added comments so the morning version of me will not want to kill the night version of me

--- src/EventPlanner.UI/Controllers/WebApi/PlannerController.cs
+++ src/EventPlanner.UI/Controllers/WebApi/PlannerController.cs
@@ -48,11 +48,14 @@ namespace EventPlanner.UI.Controllers.WebApi
         [Route("event/{eventId}/get")]
         public async Task<IActionResult> GetEventData(string eventId)
         {
+            // Get event
             var eventDto = await eventFacade.GetEvent(eventId);
+
+            // Get all users on this event
             var users = await eventFacade.GetUsersForEvent(eventId);
 
             var page = new EventPageVM();
-            page.SelectedPlaceId = 1;
+            page.SelectedPlaceId = 0;
 
             var id = ObjectId.Parse(eventId);
             page.Tables = new TableVM[eventDto.Places.Count];
@@ -61,24 +64,36 @@ namespace EventPlanner.UI.Controllers.WebApi
             var places = eventDto.Places.OrderBy(x => x.Title).ToList();
             for (int i = 0; i < places.Count; i++)
             {
+                // Users that have some choices for this place
+                // i => index of the place
                 var placeUsers = users.Where(x => x.UserEvents[id].Choices.ContainsKey(i));
+
+                // Users that do not have any choice for this place
                 var rest = users.Where(x => !placeUsers.Any(y => x.Id == y.Id));
+
+                // Add users with choices
                 var userRows = placeUsers.Select(user => new UserRowVM()
                 {
                     UserName = user.Email,
+                    // i => index of the place
                     Choices = user.UserEvents[id].Choices[i]
                 }).ToList();
+
+                // Add users without choices
                 userRows.AddRange(rest.Select(user => new UserRowVM()
                 {
                     UserName = user.Email
                 }));
 
+                // Get place we're working on
                 var place = places.ElementAt(i);
                 page.Tables[i] = new TableVM()
                 {
+                    // i => index of the place
                     Key = i,
                     Header = new HeaderVM()
                     {
+                        // Group datetimes by date and then assign to every date time
                         Dates = eventDto.Times.OrderBy(x => x).GroupBy(y => y.Date).Select(z => new DateVM()
                         {
                             Value = z.Key.ToString("dd:MM:yyyy"),
@@ -90,6 +105,7 @@ namespace EventPlanner.UI.Controllers.WebApi
 
                 page.Markers[i] = new MarkerVM()
                 {
+                    // i => index of the place
                     Key = i,
                     Position = new PositionVM()
                     {
